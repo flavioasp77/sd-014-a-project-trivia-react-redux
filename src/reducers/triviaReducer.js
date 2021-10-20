d3f1436fb1cbd6d83eb35567281831865659bdef
Robert Nascimento <robert1a7x@gmail.com>
2021-10-20 17:00:47 -0300

    Redux configurado

---
+++ src/reducers/triviaReducer.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  questions: '',
+  token: '',
+};
+
+const triviaReducer = (state = INITIAL_STATE) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default triviaReducer;

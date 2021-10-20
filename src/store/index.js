8effd4e4db88fc64539dd449bceafee3d1a7c5a1
Henrique Braga <henrique.junqueira@engenharia.ufjf.br>
2021-10-20 16:03:57 -0300

    requisito 1 ok

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+);
+
+export default store;
+
+if (window.Cypress) {
+  window.store = store;
+}

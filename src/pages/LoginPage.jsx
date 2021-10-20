dec396b186b67987b16ad9c558d680fb9ef10ff7
DanFilipe2000 <anielilipe90@gmail.com>
2021-10-20 16:17:29 -0300

    Criando paginas do Redux

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FormLogin from '../components/FormLogin';
+
+class LoginPage extends React.Component {
+  render() {
+    return (
+      <FormLogin />
+    );
+  }
+}
+
+export default LoginPage;
---
+++ src/redux/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// const store = createStore(
+	/* rootReducer */
+	// composeWithDevTools(
+		/* middleware */
+	// ),
+// );

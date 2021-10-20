3fc2891a46e7883192fe83350b75844228c15257
Flavio Pires <flavioasp77@gmail.com>
2021-10-20 17:18:07 -0300

    Componente img criado

---
+++ src/components/Img.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Img extends Component {
+  render() {
+    const {
+      id,
+      src,
+      alt,
+      className,
+    } = this.props;
+    return (
+      <img
+        src={ src }
+        alt={ alt }
+        id={ id }
+        className={ className }
+      />
+    );
+  }
+}
+
+Img.propTypes = {
+  id: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+};
+
+export default Img;

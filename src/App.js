be0d0f597236aac7b1efd297d22e8c24820b85cd
GustavoAns <gugasato9@live.com>
2021-10-20 17:38:43 -0300

    Req 2 Add Page Trivia + services

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
 import Login from './pages/login';
+import Trivia from './pages/Trivia';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/trivia" component={ Trivia } />
     </Switch>
   );
 }
---
+++ src/pages/Trivia.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+class Trivia extends Component {
+  render() {
+    return (
+      <div>
+        Trivia
+      </div>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => ({
+
+// })
+
+// const mapDispatchToProps = {
+
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Trivia)
+export default Trivia;

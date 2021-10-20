8439d249eb0be33791b6cf7b2272325a5a863fdf
Isaddora Bayma <isadora.bayma@gmail.com>
2021-10-20 17:11:45 -0300

    requisto 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route exact path="/settings" component={ Settings } />
         </Switch>
       </div>
     );

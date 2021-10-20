615ad94c2162ea6e41354a845f437980084752f5
Semnome1505 <Gabriel_1505@outlook.com>
2021-10-20 16:37:24 -0300

    Cria os metodos handleClick, handleChange e checkInputsData. O ultimo foi reaproveitado do projeto TybeWallet. Conecta o componente com o store do redux e salva os dados de nome e email nele.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loginAction } from '../actions';
 
 class Login extends React.Component {
   constructor() {
@@ -7,18 +10,53 @@ class Login extends React.Component {
     this.state = {
       name: '',
       email: '',
+      disabled: true 
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.checkInputsData = this.checkInputsData.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange({ target }) {
     const { value, name } = target;
-    this.setState({ [name]: value });
+    this.setState({
+      [name]: value,
+    }, () => this.checkInputsData());
+  }
+
+  handleClick() {
+    const { email, name } = this.state;
+    const { sendLogin } = this.props;
+
+    const data = {
+      email,
+      name
+    };
+
+    sendLogin(data);
+  }
+
+  checkInputsData() {
+    let notAllow = 0;
+    const { email, name } = this.state;
+    const emailArroba = email.split('@');
+    if (name.trim() !== '') {
+      notAllow += 1;
+    }
+    if (emailArroba.length === 2 && emailArroba[1].includes('.com')) {
+      notAllow += 1;
+    }
+    if (notAllow === 2) {
+      this.setState({ disabled: false });
+    } else {
+      this.setState({ disabled: true });
+    }
   }
 
   render() {
-    const { name, email } = this.state;
+    const { name, email, disabled } = this.state;
+
     return(
       <section>
         <br />
@@ -48,10 +86,27 @@ class Login extends React.Component {
         </label>
         <br />
         <br />
-        <button type="button" data-testid="btn-play">Jogar</button>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ disabled }
+          onClick={ this.handleClick }
+        >
+          Jogar
+        </button>
       </section>
     );
   }
 }
 
-export default Login;
+function mapDispatchToProps(dispatch) {
+  return {
+    sendLogin: (data) => dispatch(loginAction(data)), 
+  };
+}
+
+Login.propTypes = {
+  sendLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);

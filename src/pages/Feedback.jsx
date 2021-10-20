7365c01800e13c581f2cfbbd5107ee96c424e091
MarceloPalochas <almeidadearaujomarcelo@gmail.com>
2021-10-20 17:17:09 -0300

    Acredita-se que o header da tela de Feedback esta pronto

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import FeedbackHeader from '../components/FeedbackHeader';
+import getGravatar from '../helpers/getGravatar';
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      score: 0,
+      pictureURL: '',
+    };
+
+    this.cloneLocalStorageToState = this.cloneLocalStorageToState.bind(this);
+
+    document.title = 'Trivia-Feedback';
+  }
+
+  componentDidMount() {
+    this.cloneLocalStorageToState();
+  }
+
+  cloneLocalStorageToState() {
+    const playerInfo = JSON.parse(localStorage.getItem('state'));
+    const { player: { name, score, gravatarEmail } } = playerInfo;
+    this.setState({
+      name,
+      score,
+      pictureURL: getGravatar(gravatarEmail),
+    });
+  }
+
   render() {
+    const { name, score, pictureURL } = this.state;
     return (
-      <>Feedback</>
+      <>
+        <FeedbackHeader name={ name } score={ score } pictureURL={ pictureURL } />
+        <p>Feedback message</p>
+      </>
     );
   }
 }

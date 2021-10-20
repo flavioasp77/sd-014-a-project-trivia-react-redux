cf8dd7b981433ea7249604339b3d5485546d9c8a
Maurício Leite <mauricioleite1@gmail.com>
2021-10-20 20:53:06 -0300

    Adição de CSS à página de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Input from '../components/Input';
 import Button from '../components/Button';
+import logo from '../trivia.png';
+import '../css/Login.css';
 
 class Login extends Component {
   constructor() {
@@ -29,28 +31,31 @@ class Login extends Component {
     const { nameInput, emailInput } = this.state;
 
     return (
-      <div>
-        <Input
-          dataTestId="input-player-name"
-          id="name"
-          name="nameInput"
-          label="Nome"
-          onChange={ handleInput }
-        />
-        <Input
-          dataTestId="input-gravatar-email"
-          id="email"
-          name="emailInput"
-          label="Email"
-          onChange={ handleInput }
-          type="email"
-        />
-        <Button
-          dataTestId="btn-play"
-          disabled={ nameInput.length === 0 || emailInput.length === 0 }
-          onClick={ handleClick }
-          value="Jogar"
-        />
+      <div className="login__container">
+        <div className="login">
+          <img src={ logo } className="logo" alt="Logo" height="80px" />
+          <Input
+            dataTestId="input-player-name"
+            id="name"
+            name="nameInput"
+            label="Nome"
+            onChange={ handleInput }
+          />
+          <Input
+            dataTestId="input-gravatar-email"
+            id="email"
+            name="emailInput"
+            label="Email"
+            onChange={ handleInput }
+            type="email"
+          />
+          <Button
+            dataTestId="btn-play"
+            disabled={ nameInput.length === 0 || emailInput.length === 0 }
+            onClick={ handleClick }
+            value="Jogar"
+          />
+        </div>
       </div>
     );
   }

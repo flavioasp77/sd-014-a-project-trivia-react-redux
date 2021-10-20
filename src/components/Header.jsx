f0dc4dcc9dca99e61b16d648b93f7288709882e4
thiagoalmeida22 <thiagoguimaraesalmeida@gmail.com>
2021-10-20 17:52:13 -0300

    Req 4 feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import md5 from 'crypto-js/md5';
 
 class Header extends React.Component {
   constructor() {
     super();
     this.state = {
       player: {},
+      hashInfo: '',
     };
   }
 
@@ -14,20 +16,22 @@ class Header extends React.Component {
 
   playerInfoStart() {
     const player = JSON.parse(localStorage.getItem('state'));
+    const hashInfo = md5(player.gravatarEmail).toString();
     this.setState({
       player,
+      hashInfo,
     });
   }
 
   render() {
-    const { player } = this.state;
+    const { player, hashInfo } = this.state;
     console.log('Objeto player (Depois apagar) -- components/header:', player);
     return (
       <header>
         <img
           alt="Profile"
           data-testid="header-profile-picture"
-          src={ player.algumacoisa }
+          src={ `https://www.gravatar.com/avatar/${hashInfo}` }
         />
         <span data-testid="header-player-name">{ player.name }</span>
         <span data-testid="header-score">{ player.score }</span>

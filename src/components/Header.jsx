1d8839f423d989b5bb6fafdd26f00e0a0cb4bcd3
Pablo Assunção <pabloassuncao2@gmail.com>
2021-10-20 20:03:48 -0300

    Adiciona o requisito 4

---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import md5 from 'crypto-js/md5';
+
+import { getPlayerInfo } from '../services/playerInfo';
+
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      score: 0,
+      imgSrc: null,
+    };
+    this.getPlayer = this.getPlayer.bind(this);
+  }
+
+  componentDidMount() {
+    this.getPlayer();
+  }
+
+  getPlayer() {
+    const player = getPlayerInfo();
+    console.log(getPlayerInfo());
+    const { name, email, score } = player;
+    const emailMd5 = md5(email).toString();
+    this.setState({
+      name,
+      score,
+      imgSrc: `https://www.gravatar.com/avatar/${emailMd5}`,
+    });
+  }
+
+  render() {
+    const { name, score, imgSrc } = this.state;
+    return (
+      <div>
+        <img src={ imgSrc } alt={ name } data-testid="header-profile-picture" />
+        <p data-testid="header-player-name">{ name }</p>
+        <p data-testid="header-score">{ score }</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/services/triviaAPI.js
+++ src/services/triviaAPI.js
@@ -1,4 +1,4 @@
 export default async function getTriviaToken() {
   const response = await fetch('https://opentdb.com/api_token.php?command=request');
-  return localStorage.setItem('token', response.json().token);
+  return localStorage.setItem('token', JSON.stringify(response.json().token));
 }

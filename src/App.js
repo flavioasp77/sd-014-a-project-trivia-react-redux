f293feda819a19738309e2d6d7156481a0132cdf
pedrobuente <pedrobuente@gmail.com>
2021-10-20 19:23:25 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Config from './pages/Config';
+import GameScreen from './pages/GameScreen';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ Login } />
       <Route exact path="/config" component={ Config } />
+      <Route exact path="/game" component={ GameScreen } />
     </BrowserRouter>
   );
 }
---
+++ src/pages/GameScreen.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Header } from '../components/Header';
+
+export class GameScreen extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default GameScreen;

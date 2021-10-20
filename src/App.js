4487c5c1d0fe08bd637c64912edf0b5ed4fa3b5a
pedrobuente <pedrobuente@gmail.com>
2021-10-20 16:00:52 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './trivia.png';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={ Login } />
+    </BrowserRouter>
   );
 }

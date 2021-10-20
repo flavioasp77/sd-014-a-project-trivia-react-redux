3af74f6cc92115c203d0d3c3f7c101e9d5871dd6
Filipe <andradefilipe2002@gmail.com>
2021-10-20 19:31:32 -0300

    Requisito 5, FAz a requisição de perguntas para a API

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,11 +2,41 @@ import React from 'react';
 import Header from '../components/Header';
 
 class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.questRequest = this.questRequest.bind(this);
+
+    this.state = {
+      questions: [],
+    };
+  }
+
+  componentDidMount() {
+    this.questRequest();
+  }
+
+  questRequest() {
+    const token = localStorage.getItem('token');
+    try {
+      fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+        .then((request) => request.json())
+        .then((data) => this.setState({
+          questions: data.results,
+        }));
+      console.log(token);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
+    const { questions } = this.state;
+    console.log(questions);
     return (
       <main>
         <Header />
-        <h1>Main</h1>
+        <div>{questions.map((question) => <p>{question.category}</p>)}</div>
       </main>
     );
   }

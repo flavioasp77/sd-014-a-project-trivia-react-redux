b605fc18cd5425c380ca88510038d5d3fa8dcc3e
BlyNTC <isaacase@hotmail.com>
2021-10-20 17:46:16 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Config from './pages/Config';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/config" component={ Config } />
     </BrowserRouter>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { getToken } from '../services/APIrequests';
 import { saveToken } from '../services/localStorage';
 
@@ -42,6 +43,7 @@ class Login extends Component {
   }
 
   render() {
+    const { history } = this.props;
     const { disabled } = this.state;
     return (
       <form>
@@ -73,11 +75,25 @@ class Login extends Component {
           onClick={ this.handleClick }
         >
           Jogar
-
+        </button>
+        <button
+          type="button"
+          data-testid="btn-settings"
+          onClick={ () => {
+            history.push('/config');
+          } }
+        >
+          ...
         </button>
       </form>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;

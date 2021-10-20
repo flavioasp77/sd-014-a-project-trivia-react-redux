f9a1ab859e54b5b8c504d7bd92c94fd4a0f14382
Samuel Nascimento <sfsnascimento@gmail.com>
2021-10-20 17:28:31 -0300

    Requisito 3

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 data-testid="settings-title">Configurações</h1>
+      </header>
+    );
+  }
+}
+
+export default Settings;

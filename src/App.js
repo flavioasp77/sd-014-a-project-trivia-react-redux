5be47456640adab9fc19a77613380cec23efae3f
MuriloM <muriloommaia@gmail.com>
2021-10-20 16:03:36 -0300

    Finalizaçao requisito 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route } from 'react-router';
+// import logo from './trivia.png';
 import './App.css';
+import Login from './pages/Login';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
           SUA VEZ
         </p>
-      </header>
+      </header> */}
+      <Route exact path="/" component={ Login } />
     </div>
   );
 }

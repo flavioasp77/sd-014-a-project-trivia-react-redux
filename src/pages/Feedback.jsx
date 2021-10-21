79c97bb1d21b457c59d5ff325d98af3345a76b36
Hugoe-Emannuel <85767023+Hugoe-Emannuel@users.noreply.github.com>
2021-10-21 16:46:59 -0300

    aaa

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 class Feedback extends React.Component {
   render() {
-    console.log(localStorage.getItem(user.coins)
     return (
       <header>
         <h1 data-testid="feedback-text">

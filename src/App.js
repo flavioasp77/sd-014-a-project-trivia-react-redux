337aab2079467cc18f655c276b03fd00d3524814
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-10-21 18:15:29 -0300

    Adicionado requisitos 5, 6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Trivia from './pages/Trivia';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-    </Switch>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+      <Trivia />
+    </div>
   );
 }

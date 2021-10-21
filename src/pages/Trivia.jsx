5cd23843a2c1c0f0da00d0d7e0ec27b540b19b44
Lys <lysprestes@gmail.com>
2021-10-21 14:11:48 -0400

    Requisito 12 (tentativa)

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { connect } from 'react-redux';
-import Header from './Header';
+import Header from '../components/Header';
 
 class Trivia extends Component {
   render() {

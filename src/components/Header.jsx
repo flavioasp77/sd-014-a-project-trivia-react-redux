8f3771eb187a3c786f1017ac7a762743634adc01
unamednada <dias.gustavo.dias@gmail.com>
2021-10-21 14:54:24 -0300

    Instala crypto-js, cria componente genérico header e inicia gravatarAPI.js em services

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>header!</div>
+    );
+  }
+}
+
+export default Header;

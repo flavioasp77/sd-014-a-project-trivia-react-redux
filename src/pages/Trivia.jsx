d4915956394e990d945acd34ab51c66d8279ba8c
Victor Varges <victorryandias@gmail.com>
2021-10-21 17:02:18 -0300

    checking requirement 5

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
 
+import Header from '../components/Header';
+import QuestionCard from '../components/QuestionCard';
+import opentdbAPI from '../services/opentdbAPI';
+import Loading from '../components/Loading';
+
+const QUESTIONS_AMOUNT = 5;
+
 export default class Trivia extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      questions: [],
+      score: 0,
+      loading: true,
+    };
+
+    this.handleAnswer = this.handleAnswer.bind(this);
+  }
+
+  async componentDidMount() {
+    const response = await opentdbAPI.fetchQuestions(QUESTIONS_AMOUNT);
+    this.loadQuestions(response.results);
+    console.log(response.results);
+  }
+
+  loadQuestions(questions) {
+    this.setState({ questions, loading: false });
+  }
+
+  handleAnswer(answer) {
+    const { score } = this.state;
+    if (answer) {
+      this.setState({ score: score + 1 });
+    }
+  }
+
   render() {
+    const { questions, loading } = this.state;
+    if (loading) {
+      return (<Loading />);
+    }
+
+    const question = questions.shift();
+    console.log('question:', question);
     return (
-      <main id="trivia">
-        <h1>Trivia</h1>
-      </main>
+      <>
+        <Header />
+        <QuestionCard question={ question } callback={ this.handleAnswer } />
+      </>
     );
   }
 }
--- src/services/opentdbAPI.js
+++ src/services/opentdbAPI.js
@@ -3,15 +3,29 @@ import storage from './storage';
 const OPENTDB_BASEURL = 'https://opentdb.com';
 const QUESTIONS = 5;
 
+const RESPONSE_CODE_SUCESS = 0;
+// const RESPONSE_CODE_NO_RESULTS = 1;
+// const RESPONSE_CODE_INVALID_PARAMETER = 2;
+const RESPONSE_CODE_TOKEN_NOT_FOUND = 3;
+// const RESPONSE_CODE_TOKEN_EMPTY = 4;
+
 const opentdbAPI = {
   fetchToken: async () => {
     const OPENTDB_TOKEN = `${OPENTDB_BASEURL}/api_token.php?command=request`;
     try {
       const response = await (await fetch(OPENTDB_TOKEN)).json();
-      if (response.response_code !== 0) {
-        throw new Error(`response_code ${response.response_code}`);
+      switch (response.response_code) {
+      case RESPONSE_CODE_SUCESS:
+        storage.clear('token');
+        storage.write('token', response.token);
+        return;
+      case RESPONSE_CODE_TOKEN_NOT_FOUND:
+        storage.clear('token');
+        this.fetchToken();
+        return;
+      default:
+        throw new Error(`[fetchToken] error: response_code ${response.response_code}`);
       }
-      storage.write('token', response.token);
     } catch (error) {
       console.log(error);
     }
@@ -22,10 +36,16 @@ const opentdbAPI = {
     const OPENTDB_TRIVIA = `${OPENTDB_BASEURL}/api.php?amount=${amount}&token=${token}`;
     try {
       const response = await (await fetch(OPENTDB_TRIVIA)).json();
-      if (response.response_code !== 0) {
-        throw new Error(`response_code ${response.response_code}`);
+      switch (response.response_code) {
+      case RESPONSE_CODE_SUCESS:
+        return response;
+      case RESPONSE_CODE_TOKEN_NOT_FOUND:
+        storage.clear('token');
+        this.fetchToken();
+        return await this.fetchQuestions(amount);
+      default:
+        throw new Error(`[fetchQuestion] error: response_code ${response.response_code}`);
       }
-      return response;
     } catch (error) {
       console.log(error);
     }

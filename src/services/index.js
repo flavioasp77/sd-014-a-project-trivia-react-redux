cdbb020f52ca9449dd13fba94afbabb4018f312e
tiagosathler <sathler@gmail.com>
2021-10-21 13:18:18 -0300

    Requisito 2: parte b - Desenvolve a implementação das actions ao redux

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,15 @@
 const TOKEN_KEY = 'token';
 
+const URL_GET_TOKEN = 'https://opentdb.com/api_token.php?command=request';
+
 export const fetchToken = async () => {
   try {
-    const tokenRaw = await fetch('https://opentdb.com/api_token.php?command=request');
+    const response = await fetch(URL_GET_TOKEN);
+    console.log(response);
+    const tokenRaw = await response.json();
     const { token, response_code: code } = tokenRaw;
-    if (code === 0 && tokenRaw.ok) return Promise.resolve(token);
+    console.log(token);
+    if (code === 0 && response.ok) return Promise.resolve(token);
     return Promise.reject(new Error('Falha na requisição à API'));
   } catch (error) {
     return Promise.reject(error.message);

b0687841588933dbbad961a760558e4cca1732a3
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-21 17:55:28 -0300

    Render single question

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -2,22 +2,34 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchData as fetchDataAction } from '../actions';
+import Question from '../components/Question';
 
 class Play extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
   componentDidMount() {
     const { fetchData } = this.props;
-    fetchData();
+    fetchData().then(() => this.setState({ isLoading: false }));
   }
 
   render() {
     const { data } = this.props;
-    const { response_code: code } = data;
+    const { isLoading } = this.state;
+    const { response_code: code, results } = data;
+
+    if (isLoading) return <div>Loading...</div>;
 
     if (code !== 0) return <div>Token expirado!</div>;
 
     return (
       <div>
         <h1>Play</h1>
+        <Question data={ results[0] } />
       </div>
     );
   }
@@ -32,10 +44,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 Play.propTypes = {
-  data: PropTypes.shape({
-    response_code: PropTypes.number,
-    results: PropTypes.arrayOf(PropTypes.object),
-  }).isRequired,
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
   fetchData: PropTypes.func.isRequired,
 };
 

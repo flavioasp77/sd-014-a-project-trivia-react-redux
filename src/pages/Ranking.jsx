30203c626bbe0b9aaf6312af2276761333e408e2
MarceloPalochas <almeidadearaujomarcelo@gmail.com>
2021-10-21 15:43:42 -0300

    Implementa tela de Ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
+    const rankingList = JSON.parse(localStorage.getItem('ranking'))
+      .sort((a, b) => b.score - a.score);
     return (
-      <>Ranking</>
+      <>
+        <section>
+          <h1 data-testid="ranking-title">Lista de Ranking</h1>
+          {rankingList.map((player, index) => (
+            <React.Fragment key={ index }>
+              <img
+                src={ `https://www.gravatar.com/avatar/${player.picture}` }
+                alt="Imagem do seu avatar"
+                data-testid="header-profile-picture"
+              />
+              <h3 data-testid={ `player-name-${index}` }>{player.name}</h3>
+              <h4 data-testid={ `player-score-${index}` }>{player.score}</h4>
+            </React.Fragment>))}
+        </section>
+        <Link to="/">
+          <button
+            data-testid="btn-go-home"
+            type="button"
+          >
+            Ir ao Início
+          </button>
+        </Link>
+      </>
     );
   }
 }

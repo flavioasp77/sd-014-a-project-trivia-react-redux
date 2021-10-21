1b7976a24c43d2ea3e51ea231c7809b6810401e7
guiroos <guilherme.roosr@gmail.com>
2021-10-21 14:20:39 -0300

    implementando cores corretas e erradas

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -30,7 +30,10 @@ class Game extends React.Component {
   }
 
   handleChoice = () => {
-
+    document.querySelectorAll('.wrong').forEach((button) => {
+      button.style.border = '3px solid rgb(255, 0, 0)';
+    });
+    document.querySelector('.correct').style.border = '3px solid rgb(6, 240, 15)';
   }
 
   render() {

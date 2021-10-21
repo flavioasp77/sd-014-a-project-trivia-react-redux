30363fe8d4680759397463f091f41bac95851779
Silas Oliveira <silas.oliveira8@gmail.com>
2021-10-21 00:03:07 -0300

    requisito 2

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 
 class Settings extends Component {
   render() {
@@ -10,9 +9,5 @@ class Settings extends Component {
     );
   }
 }
-
-// Settings.propTypes = {
-
-// };
 
 export default Settings;

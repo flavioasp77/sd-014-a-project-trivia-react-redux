0b39a0e703562add02ae78fc8e3022b7dae35c4a
Filipe Brochier <filipe.brochier@gmail.com>
2021-10-21 16:56:17 -0300

    Finaliza requisito 2

---
+++ src/pages/Game.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>Game</span>
+      </div>
+    );
+  }
+}
+
+export default Game;

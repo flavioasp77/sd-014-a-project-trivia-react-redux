46422720d1ee56b4b015605eae37765c7632fefe
unamednada <dias.gustavo.dias@gmail.com>
2021-10-21 17:07:38 -0300

    Resolve conflito entre requisito-4 e 5

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from '../components/Header';
-
-import Header from '../components/Header';
 import QuestionCard from '../components/QuestionCard';
 import opentdbAPI from '../services/opentdbAPI';
 import Loading from '../components/Loading';

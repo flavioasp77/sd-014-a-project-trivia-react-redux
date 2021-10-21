ac770630df0d35ce6a2339d68bc3ede318168741
Filipe <andradefilipe2002@gmail.com>
2021-10-21 15:21:25 -0300

    Requisito 5, implementa um card simples para as perguntas

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -36,7 +36,26 @@ class MainPage extends React.Component {
     return (
       <main>
         <Header />
-        <div>{questions.map((question) => <p>{question.category}</p>)}</div>
+        {questions.map((question, index) => (
+          <section key={ index }>
+            <p data-testid="question-category">{question.category}</p>
+            <p data-testid="question-text">{question.question}</p>
+            <button
+              data-testid="correct-answer"
+              type="button"
+            >
+              {question.correct_answer}
+
+            </button>
+            {question.incorrect_answers.map((incorrect, ind) => (
+              <button
+                data-testid={ `wrong-answer-${ind}` }
+                type="button"
+                key={ ind }
+              >
+                {incorrect}
+              </button>))}
+          </section>))}
       </main>
     );
   }

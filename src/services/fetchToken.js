192b68a52428aaad0734181b159ea91399720e1e
Bernardo <benotargiacomo@gmail.com>
2021-10-21 17:52:56 -0300

    Requisito 2

---
+++ src/services/fetchToken.js
@@ -0,0 +1,16 @@
+const fetchToken = async () => {
+  const URL = 'https://opentdb.com/api_token.php?command=request';
+
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
+    const { token } = json;
+
+    localStorage.token = token;
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default fetchToken;

a5399d455dd6e102d5c89885f0a23d875525f992
Caio César Amorim <caiocavalcante063@gmail.com>
2021-10-21 18:05:23 -0300

    agora foi gloria deus

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,40 +1,44 @@
 /* eslint-disable react/prop-types */
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import fetchApi from '../services/triviaApi';
+import { questionsInfoThunk } from '../actions';
 
 class Questions extends Component {
   componentDidMount() {
-    const { callApi } = this.props;
-    callApi();
+    const { questionInfo } = this.props;
+    questionInfo();
   }
 
   render() {
-    const { questionResults } = this.props;
+    const { questionResults, isFetching } = this.props;
     return (
-      <div>
-        <section>
-          <h3 data-testid="question-category">
-            {
-
-            }
-          </h3>
-          <p data-testid="question-text">Pergunta</p>
-        </section>
-        <section>
-          Map para os Botões
-        </section>
-      </div>
+      isFetching ? <p>Loading</p>
+        : (
+          <div>
+            <section>
+              <h3 data-testid="question-category">
+                {
+                  questionResults.response[0].category
+                }
+              </h3>
+              <p data-testid="question-text">Pergunta</p>
+            </section>
+            <section>
+              Map para os Botões
+            </section>
+          </div>
+        )
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  callApi: (data) => dispatch(fetchApi(data)),
+  questionInfo: () => dispatch(questionsInfoThunk()),
 });
 
 const mapStateToProps = (state) => ({
-  questionResults: state.questions.results,
+  questionResults: state.questions.response,
+  isFetching: state.questions.isFetching,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Questions);
--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -2,6 +2,7 @@ import { QUESTIONS_INFO } from '../actions';
 
 const INITIAL_STATE = {
   questions: [],
+  isFetching: true,
 };
 
 function user(state = INITIAL_STATE, action) {
@@ -9,7 +10,8 @@ function user(state = INITIAL_STATE, action) {
   case QUESTIONS_INFO:
     return {
       ...state,
-      ...action.data,
+      ...action.payload,
+      isFetching: false,
     };
   default:
     return state;

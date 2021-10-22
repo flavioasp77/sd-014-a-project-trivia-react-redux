77dbc7569ef531cfa5c2ec963a1ec8536f734095
Lucas Rimis <lucasrimis@gmail.com>
2021-10-22 15:02:42 -0300

    Req 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import PlayAgainBtn from '../components/PlayAgainBtn';
 
 export default class Feedback extends Component {
   render() {
     return (
       <div data-testid="feedback-text">
         <Header />
+        <PlayAgainBtn />
       </div>
     );
   }

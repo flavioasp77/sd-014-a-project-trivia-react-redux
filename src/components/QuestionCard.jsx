7072d02e472eba441c789b98aa48e0ed75bbbff6
Marcello Pires Alves <marcello.alves@gmail.com>
2021-10-22 12:06:44 -0300

    Requisito 7.

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Button from './Button';
+import '../styles/QuestionCard.css';
+
+const CORRECT_ANSWER = 'correct-answer';
+const WRONG_ANSWER = 'wrong-answer';
 
 export default class QuestionCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   // ver https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
   shuffle(array) {
     for (let i = array.length - 1; i > 0; i -= 1) {
@@ -11,6 +21,16 @@ export default class QuestionCard extends React.Component {
       [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
+  }
+
+  handleClick(correct) {
+    const { callback } = this.props;
+    const rightButton = document.querySelector('[data-testid="correct-answer"]');
+    const wrongButtons = document.querySelectorAll('[data-testid*="wrong-answer"]');
+    rightButton.className = CORRECT_ANSWER;
+    wrongButtons.forEach((button) => { button.className = WRONG_ANSWER; });
+
+    callback(correct);
   }
 
   render() {
@@ -33,16 +53,16 @@ export default class QuestionCard extends React.Component {
             const correct = option === correctAnswer;
             return (<Button
               key={ index }
-              onClick={ () => callback(correct) }
+              onClick={ () => this.handleClick(correct) }
               submit={ false }
               testid={
                 correct
-                  ? 'correct-answer'
+                  ? CORRECT_ANSWER
                   : `wrong-answer-${incorrectAnswers.indexOf(option)}`
               }
               value={ option }
             />);
-          })}
+          }) }
         </div>
       </div>
     );

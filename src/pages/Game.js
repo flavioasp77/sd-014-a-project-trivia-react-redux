9ceead8416360b37f37a26a6e68860e11985313e
Dogl4 <doougllas@hotmail.com.br>
2021-10-22 20:01:10 -0300

    Requisito 6 Concluido | Renderiza apenas uma pergunta por vez.

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -6,26 +6,43 @@ import Question from '../components/question';
 import Header from '../components/Header';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      index: 0,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { index } = this.state;
+    const { questions } = this.props;
+    if (index < (questions.length - 1)) { this.setState({ index: index + 1 }); }
+  }
+
   render() {
     const { questions } = this.props;
+    const { index } = this.state;
     return (
       <main>
         <Header />
         { console.log(this.props) }
-        { questions && questions.map((question, index) => (
-          <Question key={ index } questionCurrent={ question } />)) }
+        { questions && <Question key={ index } questionCurrent={ questions[index] } /> }
+        <button type="button" onClick={ this.handleClick }>Próxima pergunta</button>
       </main>
     );
   }
 }
 
-Game.propTypes = {
-  setApiTrivia: PropTypes.func,
-}.isRequired;
-
 const mapStateToProps = (state) => ({
   questions: state.trivia.questions,
   state,
 });
+
+Game.propTypes = {
+  questions: PropTypes.array,
+}.isRequired;
 
 export default connect(mapStateToProps)(Game);

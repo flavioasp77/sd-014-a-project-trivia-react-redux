15c41b401ab27b7ef1881e12343955d0a1350c42
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-10-22 14:26:31 -0300

    Adicionado requisitos 7 e 10

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import '../style/style.css';
+
 export default class Trivia extends Component {
   constructor() {
     super();
@@ -7,10 +9,13 @@ export default class Trivia extends Component {
     this.state = {
       questions: [],
       index: 0,
+      className: false,
     };
 
     this.fetchApi = this.fetchApi.bind(this);
     this.sortArray = this.sortArray.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.refreshPage = this.refreshPage.bind(this);
   }
 
   componentDidMount() {
@@ -19,14 +24,12 @@ export default class Trivia extends Component {
 
   sortArray() {
     const { questions, index } = this.state;
-    const {
-      correct_answer: correct,
-      incorrect_answers: incorrect } = questions[index];
+    const { correct_answer: correct, incorrect_answers: incorrect } = questions[index];
     return [...incorrect, correct].sort();
   }
 
   async fetchApi() {
-    const TOKEN = 'c6a9bd5af74a4b3810ba761019fbfe0ee4422118ac3abf40bdd2e6b9c1dc73ea';
+    const TOKEN = '65553a27dab8a0319076652051e7fd436618c2ad3ee3cee08e74dcda21ce6ecd';
     const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${TOKEN}`);
     const result = await response.json();
     // console.log('result:', result);
@@ -35,31 +38,69 @@ export default class Trivia extends Component {
     });
   }
 
+  handleClick() {
+    this.setState({ className: true });
+  }
+
+  styleSelectedMoreAlternatives() {
+    const { questions, index, className } = this.state;
+    return this.sortArray().map((atual, indice) => {
+      const correctOrWrong = atual === questions[index].correct_answer;
+      const button = (
+        <button
+          type="button"
+          key={ indice }
+          data-testid={ correctOrWrong ? 'correct-answer' : `wrong-answer-${indice}` }
+          className={ className && `button-${correctOrWrong ? 'correct' : 'wrong'}` }
+          onClick={ this.handleClick }
+        >
+          {atual}
+        </button>
+      );
+      console.log(className);
+      return button;
+    });
+  }
+
+  refreshPage() {
+    window.location.reload();
+  }
+
+  nextQuestion() {
+    const { className } = this.state;
+    if (className) {
+      const button = (
+        <button
+          type="button"
+          data-testid="btn-next"
+          onClick={ this.refreshPage }
+        >
+          Próxima
+        </button>
+      );
+      return button;
+    }
+  }
+
   render() {
     const { questions, index } = this.state;
-    console.log('Array questions:', questions, index);
+    // console.log('Array questions:', questions, index);
 
     return (
       <main>
         <h1>Jogo</h1>
         {questions.length > 0 && (
           <div>
-            <p data-testid="question-category">{ questions[index].category }</p>
+            <p data-testid="question-category">{questions[index].category}</p>
             <p data-testid="question-text">{questions[index].question}</p>
-            {this.sortArray().map((atual, indice) => (
-              <button
-                type="button"
-                key={ indice }
-                data-testid={
-                  atual === questions[index].correct_answer
-                    ? 'correct-answer'
-                    : `wrong-answer-${indice}`
-                }
-              >
-                { atual }
-              </button>
-            ))}
-          </div>)}
+            <div>
+              {this.styleSelectedMoreAlternatives()}
+            </div>
+            <div>
+              {this.nextQuestion()}
+            </div>
+          </div>
+        )}
       </main>
     );
   }

6e153f02d5e438fcb44b99a67b5aeb8fa6fbaec7
Amanda Zotelli <amzotelli@gmail.com>
2021-10-21 21:13:02 -0300

    metade do caminho requisito 5

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import md5 from 'crypto-js/md5';
+
 import Header from '../components/Header';
+import { getQuestions } from '../services/requests';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    getQuestions();
+  }
+
   render() {
     const { player } = JSON.parse(localStorage.getItem('state'));
     const userHash = md5(player.gravatarEmail).toString();
     return (
-      <Header player={ player.name } score="0" src={ `https://www.gravatar.com/avatar/${userHash}` } />
+      <main>
+        <Header player={ player.name } score="0" src={ `https://www.gravatar.com/avatar/${userHash}` } />
+        <div>
+          <h4 data-testid="question-category">categoria</h4>
+          <p data-testid="question-text">pergunta</p>
+        </div>
+      </main>
     );
   }
 }
 
 export default Game;
+
+// * A pergunta e suas alternativas de resposta devem ser recebidas da API do Trivia
+
+//   * O texto com as alternativas devem ser exibidos seguindo as regras abaixo:
+//     * O elemento com a alternativa correta deve possuir o atributo `data-testid` com o valor `correct-answer`
+//     * Os elementos com as alternativas incorretas devem possuir o atributo `data-testid` com o valor `wrong-answer-${index}`, com `${index}` iniciando com o valor `0`
+//     * As alternativas devem ser exibidas em ordem aleatória
+//     * Dica: utilize botões (`<button/>`) para as alternativas

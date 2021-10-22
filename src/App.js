9892a30abf604d405376a9f71f248e392e2847ee
raffrasson <raf.frasson@gmail.com>
2021-10-22 18:19:37 -0300

    Adds RankingCard component and Ranking page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router';
 import Login from './pages/Login';
 import Play from './pages/Play';
 import Settings from './pages/Settings';
+import Ranking from './pages/Ranking';
 
 class App extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Route exact path="/" component={ Login } />
         <Route path="/play" component={ Play } />
         <Route path="/settings" component={ Settings } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     );
   }
---
+++ src/components/RankingCard.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class RankingCard extends Component {
+  render() {
+    const { name, score, hash, index } = this.props;
+    const gravatar = `https://www.gravatar.com/avatar/${hash}`;
+    return (
+      <div className="ranking-card">
+        <img
+          alt="Player Gravatar"
+          className="ranking-profile-picture"
+          src={ gravatar }
+        />
+        <h3 data-testid={ `player-name-${index}` }>
+          Nome:
+          {' '}
+          {name}
+        </h3>
+        <h3 data-testid={ `player-score-${index}` }>
+          Pontuação:
+          {' '}
+          {score}
+        </h3>
+      </div>
+    );
+  }
+}
+
+RankingCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  score: PropTypes.string.isRequired,
+  hash: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default (RankingCard);

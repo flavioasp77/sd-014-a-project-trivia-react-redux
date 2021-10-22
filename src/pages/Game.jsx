3af4c17d0d1907a4cbed26f71afc8829f406617e
Bernardo <benotargiacomo@gmail.com>
2021-10-21 21:02:56 -0300

    Requisito 4

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Game extends Component {
   render() {
+    const { userName, thumbnail } = this.props;
+
     return (
-      <div>Game</div>
+      <header>
+        <div>
+          <img
+            data-testid="header-profile-picture"
+            src={ thumbnail }
+            alt={ `${userName} thumbnail` }
+          />
+          <span data-testid="header-player-name">
+            { userName }
+          </span>
+        </div>
+        <span data-testid="header-score"> Placar: 0</span>
+      </header>
     );
   }
 }
 
-export default Game;
+Game.propTypes = {
+  thumbnail: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userName: state.player.name,
+  thumbnail: state.player.thumbnail,
+});
+
+export default connect(mapStateToProps, null)(Game);

935fd27b28007238e0e758f057740c0d4b41d283
Filipe Brochier <filipe.brochier@gmail.com>
2021-10-22 16:15:45 -0300

    Alteraçao na funcao fetchAPI

--- src/services/apiGame.js
+++ src/services/apiGame.js
@@ -1,5 +1,5 @@
 export default async function fetchAPI() {
-  const token = localStorage.getItem('token');
+  const token = await localStorage.getItem('token');
   const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
   const game = await response.json();
   return game.results;

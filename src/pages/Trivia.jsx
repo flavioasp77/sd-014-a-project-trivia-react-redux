7bba510320c5a350c8cac1ec55b673d7dce1aee5
Daniel Kim Nakano <danielknakano@gmail.com>
2021-10-22 15:25:17 -0300

    finalizando 4 e mergeando

--- src/pages/Trivia.jsx
+++ src/pages/Trivia.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 import '../style/style.css';
 
@@ -88,6 +89,7 @@ export default class Trivia extends Component {
 
     return (
       <main>
+        <Header />
         <h1>Jogo</h1>
         {questions.length > 0 && (
           <div>

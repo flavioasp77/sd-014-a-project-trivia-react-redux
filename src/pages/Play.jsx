216bface21d3c66eca30cd994c1e46d7b91e610e
Gustavo Dolzan <gudolzan@gmail.com>
2021-10-22 03:35:43 -0300

    Add style to "expired token" message

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import QuestionCard from '../components/QuestionCard';
+import '../styles/Play.css';
 
 class Play extends Component {
   render() {
     const { data } = this.props;
     const { response_code: responseCode, results } = data;
 
-    if (responseCode !== 0) return <div>Token expirado!</div>;
+    if (responseCode !== 0) {
+      return (
+        <div className="container">
+          <p className="text-center">Token expirado!</p>
+          <Link className="btn-return" to="/">
+            VOLTAR
+          </Link>
+        </div>
+      );
+    }
 
     return (
       <div>

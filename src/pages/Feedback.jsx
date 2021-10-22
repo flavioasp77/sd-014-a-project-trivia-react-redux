743a2de1c084b36b7240667efa131f9d0ce6e4e3
César Meira <cesarmeirasouza@gmail.com>
2021-10-22 01:43:28 -0300

    Commit pra poder dar pull :|

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -10,9 +10,11 @@ class Feedback extends React.Component {
       name: '',
       score: 0,
       pictureURL: '',
+      assertions: 0,
     };
 
     this.cloneLocalStorageToState = this.cloneLocalStorageToState.bind(this);
+    this.messageFeedback = this.messageFeedback.bind(this);
 
     document.title = 'Trivia-Feedback';
   }
@@ -23,20 +25,31 @@ class Feedback extends React.Component {
 
   cloneLocalStorageToState() {
     const playerInfo = JSON.parse(localStorage.getItem('state'));
-    const { player: { name, score, gravatarEmail } } = playerInfo;
+    const { player: { name, score, gravatarEmail, assertions } } = playerInfo;
     this.setState({
       name,
       score,
       pictureURL: getGravatar(gravatarEmail),
+      assertions,
     });
   }
 
+  messageFeedback() {
+    const { score } = this.state;
+    const NUMBER_OF_HITS = 3;
+    return (score < NUMBER_OF_HITS) ? 'Podia ser melhor...' : 'Mandou bem!';
+  }
+  
   render() {
-    const { name, score, pictureURL } = this.state;
+    const { name, score, pictureURL, assertions } = this.state;
+    const questao = assertions === 1 ? 'questão' : 'questões';
     return (
       <>
         <Header name={ name } score={ score } pictureURL={ pictureURL } />
-        <p>Feedback message</p>
+        <h1>Mensagem motivacional</h1>
+        <br></br>
+        <h2 data-testid="feedback-total-question">Você acertou { assertions } { questao }!</h2>
+        <h2 data-testid="feedback-total-score">Um total de { score } pontos.</h2>
       </>
     );
   }

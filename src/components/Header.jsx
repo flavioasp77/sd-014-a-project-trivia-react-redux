767671c72e7aec6d069de338bcdbfa521bdd5bc4
AlineCarolina <eualine2017@gmail.com>
2021-10-22 20:11:30 -0300

    Requisito 12 - em andamento

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,41 @@ import React, { Component } from 'react';
 import { MD5 } from 'crypto-js';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { fetchApiGravatar } from '../services';
 
 class Header extends Component {
-  gravatarAvatar() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      src: '',
+    };
+    this.gravatarAvatar = this.gravatarAvatar.bind(this);
+  }
+
+  componentDidMount() {
+    this.gravatarAvatar();
+  }
+
+  async gravatarAvatar() {
     const { gravatarEmail } = this.props;
     const hash = MD5(gravatarEmail).toString();
-    return `https://www.gravatar.com/avatar/${hash}`;
+    const img = await fetchApiGravatar(hash);
+    this.setState({
+      src: img,
+    });
   }
 
   render() {
     const { name } = this.props;
+    const { src } = this.state;
     const score = 0;
     return (
       <div>
         <div>
           <img
+            key={ name }
             data-testid="header-profile-picture"
-            src={ this.gravatarAvatar() }
+            src={ src }
             alt={ `${name} avatar` }
           />
           <div>
--- src/services/index.js
+++ src/services/index.js
@@ -13,3 +13,10 @@ export async function fetchQuestions(token) {
   const results = await response.results;
   return results;
 }
+
+export async function fetchApiGravatar(email) {
+  const URL = `https://www.gravatar.com/avatar/${email}`;
+  const request = await fetch(URL);
+  const response = await request.url;
+  return response;
+}

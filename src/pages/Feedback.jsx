57f3fdb608b1c592fd8cc60de119e5b274db8ab4
Cristhyane Araldi <cristhyane.araldi@hotmail.com>
2021-10-22 11:36:13 -0300

    Requisito 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Feedback extends Component {
   render() {
+    const { name, source } = this.props;
+    const state = localStorage.getItem('state');
+    const { player: { score } } = JSON.parse(state);
     return (
-      <h1 data-testid="feedback-text">Feedback</h1>
+      <div>
+        <h1 data-testid="feedback-text">Feedback</h1>
+        <header>
+          <h3 data-testid="header-player-name">{ name }</h3>
+          <img
+            data-testid="header-profile-picture"
+            src={ source }
+            alt="gravatar"
+          />
+          <p>
+            Pontuação final:
+            <span data-testid="header-score">
+              { score }
+            </span>
+          </p>
+        </header>
+      </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  name: PropTypes.string.isRequired,
+  // score: PropTypes.number.isRequired,
+  source: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  name: state.player.name,
+  email: state.player.email,
+  // score: state.player.score,
+  source: state.player.source.url,
+});
+
+export default connect(mapStateToProps, null)(Feedback);

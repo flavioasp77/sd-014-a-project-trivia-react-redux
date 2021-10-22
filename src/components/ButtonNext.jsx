9fa28d3347ebaaf9304b1c7876e8c4120883c600
jagrunewald <je_grunewald@hotmail.com>
2021-10-22 19:51:17 -0300

    Requisito 10

---
+++ src/components/ButtonNext.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ButtonNext extends Component {
+  render() {
+    const { onClick } = this.props;
+    return (
+      <button
+        type="button"
+        data-testid="btn-next"
+        onClick={ onClick }
+      >
+        Próxima Questão
+      </button>
+    );
+  }
+}
+
+ButtonNext.propTypes = {
+  onClick: PropTypes.number.isRequired,
+};

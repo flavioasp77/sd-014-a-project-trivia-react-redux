d085fda1997c4608dd49a2f3f4b29c35e938ab10
Filipe Brochier <filipe.brochier@gmail.com>
2021-10-22 17:40:44 -0300

    Requisito 5 finalizado

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,45 +6,46 @@ import Header from '../components/Header';
 class Game extends React.Component {
   render() {
     const { questions } = this.props;
-    const { question, category, correctAnswer, incorrectAnswer } = questions;
+    // const { question, category, correct_answer, incorrect_answers } = questions;
     return (
       <div>
         <Header />
-        <div>
-          <h1>Question:</h1>
-          <h2 data-testid="question-text">{ question }</h2>
-          <h3>Category:</h3>
-          <h4 data-testid="question-category">{ category }</h4>
-          <form>
-            <p>Alternatives:</p>
-            <label htmlFor="correct-answer">
-              <li data-testid="correct-answer">{ correctAnswer }</li>
-              <input type="checkbox" id="correct-answer" />
-            </label>
-            { incorrectAnswer.map((answer, index) => (
-              <label htmlFor="incorrect-answer" key="incorrect-answer">
-                <li data-testid={ `wrong-answer-${index}` }>{ answer }</li>
-                <input type="checkbox" id="incorrect-answer" />
-              </label>
-            ))}
-            <input
-              type="button"
-              value="Enviar"
-            />
-          </form>
-        </div>
+        {questions.map((question, index) => (
+          <div key={ index }>
+            <h1>
+              <span data-testid="question-category">{question.category}</span>
+            </h1>
+            <h2>
+              <span data-testid="question-text">{question.question}</span>
+            </h2>
+            <ul>
+              <li>
+                <button
+                  data-testid="correct-answer"
+                  type="button"
+                >
+                  { question.correct_answer }
+                </button>
+              </li>
+              {question.incorrect_answers.map((incorrect, ind) => (
+                <li key={ ind }>
+
+                  <button
+                    data-testid={ `wrong-answer-${ind}` }
+                    type="button"
+                  >
+                    {incorrect}
+                  </button>
+                </li>))}
+            </ul>
+          </div>))}
       </div>
     );
   }
 }
 
 Game.propTypes = {
-  questions: PropTypes.shape({
-    question: PropTypes.string,
-    category: PropTypes.string,
-    correctAnswer: PropTypes.string,
-    incorrectAnswer: PropTypes.arrayOf(PropTypes.string),
-  }).isRequired,
+  questions: PropTypes.arrayOf(Object).isRequired,
 };
 
 const mapStateToProps = (state) => ({

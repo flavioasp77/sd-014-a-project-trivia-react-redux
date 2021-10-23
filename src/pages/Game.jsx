6d79a061b7f73b45572735aa9426e07a87d8b51c
Ilan Aragão <ilan_aragao@hotmail.com>
2021-10-23 14:01:10 -0300

    Adiciona lógica do timer / finaliza requisito

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,23 +6,50 @@ import Header from '../components/Header';
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.questionMapper = this.questionMapper.bind(this);
-    this.questionClick = this.questionClick.bind(this);
-    this.questionRenderer = this.questionRenderer.bind(this);
 
     this.state = {
-      questionState: [
-        false,
-        true,
-        true,
-        true,
-        true,
-      ],
+      questionState: [false, true, true, true, true],
+      timer: 30,
     };
+
+    this.questionMapper = this.questionMapper.bind(this);
+    this.questionClick = this.questionClick.bind(this);
+    this.questionRender = this.questionRender.bind(this);
+    this.timer = this.timer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.disableButton = this.disableButton.bind(this);
+  }
+
+  componentDidMount() {
+    this.timer();
+  }
+
+  timer() {
+    const ONE_SECOND = 1000;
+    setInterval(() => {
+      this.setState((prevState) => ({
+        timer: prevState.timer - 1,
+      }));
+    }, ONE_SECOND);
+  }
+
+  resetTimer() {
+    this.setState({
+      timer: 30,
+    });
+  }
+
+  disableButton() {
+    const { timer } = this.state;
+    if (timer <= 0) {
+      return true;
+    }
+    return false;
   }
 
   questionClick({ target }) {
     const { questionState } = this.state;
+    this.resetTimer();
     const questsStatus = questionState.map((quest, index) => {
       if (index === parseInt(target.name, 10) + 1) {
         return false;
@@ -35,7 +62,7 @@ class Game extends React.Component {
     });
   }
 
-  questionRenderer() {
+  questionRender() {
     const { questionState } = this.state;
     const questionArray = this.questionMapper();
     const shouldRender = questionArray.map((elem, index) => {
@@ -62,32 +89,43 @@ class Game extends React.Component {
               data-testid="correct-answer"
               type="button"
               onClick={ this.questionClick }
+              disabled={ this.disableButton() }
             >
-              { question.correct_answer }
+              {question.correct_answer}
             </button>
           </li>
           {question.incorrect_answers.map((incorrect, ind) => (
             <li key={ ind }>
-
               <button
                 name={ index }
                 data-testid={ `wrong-answer-${ind}` }
                 type="button"
                 onClick={ this.questionClick }
+                disabled={ this.disableButton() }
               >
                 {incorrect}
               </button>
-            </li>))}
+            </li>
+          ))}
         </ul>
-      </div>));
+      </div>
+    ));
     return allQuestions;
   }
 
   render() {
+    const { timer } = this.state;
     return (
       <div>
         <Header />
-        <this.questionRenderer />
+        <div>
+          <h1>
+            <span data-testid="timer">{timer}</span>
+          </h1>
+          <div>
+            {this.questionRender()}
+          </div>
+        </div>
       </div>
     );
   }

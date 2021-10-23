d63a245843a422369e66664a7d4d15f4de959f5b
Isaac Matheus <85703133+BlyNTC@users.noreply.github.com>
2021-10-23 00:34:40 -0300

    cria componente Feedback

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Feedback extends Component {
+  render() {
+    return (
+      <div data-testid="feedback-text">
+        FEEDBACK AQ !!!!
+      </div>
+    )
+  }
+}

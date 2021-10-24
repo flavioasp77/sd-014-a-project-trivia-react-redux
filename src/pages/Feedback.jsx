9dca31844f27466841f737c9727bab88b06d297c
Marcello Pires Alves <marcello.alves@gmail.com>
2021-10-23 23:12:27 -0300

    Requisito 13.

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default class Feedback extends React.Component {
+const LOW_SCORE_MESSAGE = 'Podia ser melhor...';
+const HIGH_SCORE_MESSAGE = 'Mandou bem!';
+const HIGH_SCORE_THRESHOLD = 3;
+
+class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
     return (
-      <main id="feedback-text">
+      <main>
         <Header />
-        <h1 data-testid="feedback-text">Feedback</h1>
+        <h1 data-testid="feedback-text">
+          { assertions >= HIGH_SCORE_THRESHOLD ? HIGH_SCORE_MESSAGE : LOW_SCORE_MESSAGE }
+        </h1>
         <Footer />
       </main>
     );
   }
 }
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);

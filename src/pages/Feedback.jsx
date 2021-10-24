8d0088016881425da213e77f664e571c5b0e5970
Marcello Pires Alves <marcello.alves@gmail.com>
2021-10-23 21:13:30 -0300

    Requisito 12.

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default class Feedback extends React.Component {
   render() {
     return (
       <main id="feedback-text">
+        <Header />
         <h1 data-testid="feedback-text">Feedback</h1>
+        <Footer />
       </main>
     );
   }

739a10aee3de1202497907724c7f2c9d2ef5153a
Marcello Pires Alves <marcello.alves@gmail.com>
2021-10-23 21:13:01 -0300

    Requisito 11.

---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Feedback extends React.Component {
+  render() {
+    return (
+      <main id="feedback-text">
+        <h1 data-testid="feedback-text">Feedback</h1>
+      </main>
+    );
+  }
+}

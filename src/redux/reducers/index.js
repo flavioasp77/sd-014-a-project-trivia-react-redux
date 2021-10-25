4bd199ff9ae6e61599b1fc532393ffbfa98d36a1
gsdu-arte <gabi15duarte@hotmail.com>
2021-10-25 17:34:31 -0300

    Requisito 8

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import player from './userReducer';
+import game from './gameReducer';
 
 const rootReducer = combineReducers({
   player,
+  game,
 });
 
 export default rootReducer;

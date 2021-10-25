376df94a7fb333d1098e279df9549489df8b48f7
Caio César Amorim <caiocavalcante063@gmail.com>
2021-10-25 16:23:17 -0300

    cumpre requisitos 13 e 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -3,10 +3,25 @@ import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const playerStorage = JSON.parse(localStorage.getItem('state'));
+    const { assertions, score } = playerStorage.player;
+    const ASSERTIONS_NUMBER = 3;
+    const message = assertions >= ASSERTIONS_NUMBER ? 'Mandou bem!'
+      : 'Podia ser melhor...';
     return (
       <>
         <Header />
-        <p data-testid="feedback-text">Parabéns</p>
+        <h2 data-testid="feedback-text">{ message }</h2>
+        <span>
+          Sua pontuação:
+          {' '}
+          <span data-testid="feedback-total-score">{ score }</span>
+        </span>
+        <span>
+          Número de acertos:
+          {' '}
+          <span data-testid="feedback-total-question">{ assertions }</span>
+        </span>
       </>
     );
   }

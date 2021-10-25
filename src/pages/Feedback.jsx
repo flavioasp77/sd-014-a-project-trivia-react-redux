1e22e554bdf728534f809c377cedb2b2878b37bf
Caio César Amorim <caiocavalcante063@gmail.com>
2021-10-25 16:53:05 -0300

    fix lint errors

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {

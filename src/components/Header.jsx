b99bd1a840a2178eef968029d8bb555e2be09d21
Écio Ferraz <ecioferraz@hotmail.com>
2021-10-25 15:50:05 -0300

    req 12

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { readLocalStorage } from '../services/util';
 
 class Header extends Component { // comentario
   render() {
     const { img, nome } = this.props;
+    const player = readLocalStorage('state');
+    console.log(player);
     return (
       <header>
         <img src={ img } data-testid="header-profile-picture" alt="gravatar" />
         <span data-testid="header-player-name">{ nome }</span>
-        <p data-testid="header-score">0</p>
+        <p data-testid="header-score">{ player.player.score }</p>
       </header>
     );
   }

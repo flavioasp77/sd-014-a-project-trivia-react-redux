dcd1f14736b1e643b149a1483dc9a7a6a436bb8b
Lys <lysprestes@gmail.com>
2021-10-25 17:24:48 -0400

    Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -4,13 +4,19 @@ import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
-    const { player: { assertions } } = JSON.parse(localStorage.getItem('state'));
+    const { player: { assertions, score } } = JSON.parse(localStorage.getItem('state'));
     const minimalAssertion = 3;
     return (
       <>
         <Header />
         <div data-testid="feedback-text">
-          {assertions >= minimalAssertion ? 'Mandou bem!' : 'Podia ser melhor...' }
+          { assertions >= minimalAssertion ? 'Mandou bem!' : 'Podia ser melhor...' }
+        </div>
+        <div data-testid="feedback-total-question">
+          { assertions }
+        </div>
+        <div data-testid="feedback-total-score">
+          { score }
         </div>
       </>
     );

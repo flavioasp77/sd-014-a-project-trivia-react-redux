f2adf520a021287525f547444571a24d89b69f2a
Caio César <caiovg32@gmail.com>
2021-10-25 16:05:17 -0300

    teste inicial

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class Ranking extends React.Component {
+class Ranking extends Component {
   render() {
+    const orderedRanking = ranking.sort((a, b) => b.score - a.score);
     return (
-      <main>
-        <h1 data-testid="ranking-title">
-          Ranking
-        </h1>
-      </main>
+      <div>
+        <p data-testid="ranking-title">Ranking</p>
+        <ul>
+          { orderedRanking.map((player, index) => (
+            <li key={ index }>
+              <img src={ player.gravatarUrl } alt={ player.name } />
+              <p data-testid={ `player-name-${index}` }>{ player.name }</p>
+              <p data-testid={ `player-score-${index}` }>{ player.score }</p>
+            </li>
+          )) }
+        </ul>
+        <Link to="/" data-testid="btn-go-home">
+          <button type="button">Tela inicial</button>
+        </Link>
+      </div>
     );
   }
 }

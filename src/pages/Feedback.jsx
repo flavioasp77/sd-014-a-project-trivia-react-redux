a27decdd828cd0da52c63c2c4c5b4c4b816dc477
Écio Ferraz <ecioferraz@hotmail.com>
2021-10-25 16:16:07 -0300

    req 13 e 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { readLocalStorage } from '../services/util';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.feedbackMessage = this.feedbackMessage.bind(this);
+  }
+
+  feedbackMessage() {
+    const ASSERTION = 3;
+    const player = readLocalStorage('state');
+    if (player.player.assertions < ASSERTION) {
+      return (
+        <>
+          <p data-testid="feedback-text">Podia ser melhor...</p>
+          <p data-testid="feedback-total-score">{ player.player.score }</p>
+          <p data-testid="feedback-total-question">{ player.player.assertions }</p>
+        </>
+      );
+    }
+    if (player.player.assertions >= ASSERTION) {
+      return (
+        <>
+          <p data-testid="feedback-text">Mandou bem!</p>
+          <p data-testid="feedback-total-score">{ player.player.score }</p>
+          <p data-testid="feedback-total-question">{ player.player.assertions }</p>
+        </>
+      );
+    }
+  }
+
   render() {
     return (
       <div data-testid="feedback-text">
         <Header />
+        { this.feedbackMessage() }
       </div>
     );
   }

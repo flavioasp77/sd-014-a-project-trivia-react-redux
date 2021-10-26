fec0fbacbd47e5fba52308f2c8541ce439185192
Pally <francisca.bezerr@gmail.com>
2021-10-25 23:15:29 -0300

    questao 13 ok

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { currentScore } = this.props;
+    const { assertions } = JSON.parse(localStorage.getItem('state')).player;
+    const numbFeed = 3;
+    const feedback = assertions >= numbFeed ? 'Mandou bem!'
+      : 'Podia ser melhor...';
     return (
       <>
         <header>
           <Header />
         </header>
-        <div data-testid="feedback-text">FEEDBACK</div>
+        <h1 data-testid="feedback-text">{feedback}</h1>
+        <h2 data-testid="feedback-total-question">
+          {assertions}
+        </h2>
+        <h2 data-testid="feedback-total-score">{currentScore}</h2>
+
       </>
     );
   }
 }
-
+Feedback.propTypes = {
+  currentScore: PropTypes.number.isRequired,
+};
 export default Feedback;

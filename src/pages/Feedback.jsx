510bb61d228f3fbdb11c4b4313efabb03e157895
paulodetasso <xcloudgameemail@gmail.com>
2021-10-26 16:18:59 -0300

    Atualiza css

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import './feedback.css';
 
 export default class Feedback extends Component {
   render() {
@@ -10,31 +11,44 @@ export default class Feedback extends Component {
     const message = assertions >= ASSERTIONS_NUMBER ? 'Mandou bem!'
       : 'Podia ser melhor...';
     return (
-      <>
+      <section>
         <Header />
-        <h2 data-testid="feedback-text">{ message }</h2>
-        <span>
-          Sua pontuação:
-          {' '}
-          <span data-testid="feedback-total-score">{ score }</span>
-        </span>
-        <span>
-          Número de acertos:
-          {' '}
-          <span data-testid="feedback-total-question">{ assertions }</span>
-        </span>
-        <Link to="/">
-          <button
-            type="button"
-            data-testid="btn-play-again"
-          >
-            Jogar novamente
-          </button>
-        </Link>
-        <Link to="/ranking">
-          <button type="button" data-testid="btn-ranking">Ver Ranking</button>
-        </Link>
-      </>
+        <div className="feedback">
+          <h2 data-testid="feedback-text" className="title">{ message }</h2>
+          <span>
+            <h4 className="score" data-testid="feedback-total-score">
+              Sua pontuação:
+              {' '}
+              { score }
+            </h4>
+            <h4 className="assertions" data-testid="feedback-total-question">
+              Número de acertos:
+              {' '}
+              { assertions }
+            </h4>
+          </span>
+          <div className="buttons-feedback">
+            <Link to="/">
+              <button
+                className="btn-feedback"
+                type="button"
+                data-testid="btn-play-again"
+              >
+                Jogar novamente
+              </button>
+            </Link>
+            <Link to="/ranking">
+              <button
+                type="button"
+                className="btn-feedback"
+                data-testid="btn-ranking"
+              >
+                Ver Ranking
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
     );
   }
 }

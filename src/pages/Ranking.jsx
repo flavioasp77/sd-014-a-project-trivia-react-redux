c9cb9228484c12fe3902fc3c933505c2d491ef81
Heitor SF <heitor.sf5@gmail.com>
2021-10-26 17:56:47 -0300

    req 17 and 18 done. 17 NEEDS 18 to be done to pass the tests - brilliant.

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,90 @@
 import React, { Component } from 'react';
+import MD5 from 'crypto-js/md5';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 export default class Ranking extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      score: 0,
+      picture: '',
+      loading: true,
+    };
+
+    this.rankingFetcher = this.rankingFetcher.bind(this);
+    this.rankingSetter = this.rankingSetter.bind(this);
+    this.playerFetcher = this.playerFetcher.bind(this);
+  }
+
+  componentDidMount() {
+    this.playerFetcher();
+  }
+
+  rankingSetter() {
+    const { name, score, picture } = this.state;
+    const player = { name, score, picture };
+    const triviaRanking = JSON.parse(localStorage.getItem('ranking'));
+    if (triviaRanking === null) {
+      localStorage.setItem('ranking', JSON.stringify([player]));
+    } else if (triviaRanking) {
+      triviaRanking.push(player);
+      const sortedRankings = triviaRanking
+        .map((p) => p)
+        .sort((a, b) => b.score - a.score);
+      localStorage.setItem('ranking', JSON.stringify(sortedRankings));
+    }
+
+    this.setState({
+      loading: false,
+    });
+  }
+
+  rankingFetcher() {
+    const localRankings = JSON.parse(localStorage.getItem('ranking'));
+    const rankList = localRankings.map((player, index) => {
+      const { name, score, picture } = player;
+      return (
+        <li key={ index }>
+          <p data-testid={ `player-name-${index}` }>{name}</p>
+          <p data-testid={ `player-score-${index}` }>{score}</p>
+          <img src={ picture } alt="Gravatar Fail" />
+        </li>
+      );
+    });
+
+    return (
+      <ol>
+        { rankList }
+      </ol>
+    );
+  }
+
+  playerFetcher() {
+    const { player } = JSON.parse(localStorage.getItem('state'));
+    const { username, score, gravatarEmail } = player;
+    const hash = MD5(gravatarEmail).toString();
+    const gravatarConverter = `https://www.gravatar.com/avatar/${hash}`;
+    this.setState({
+      name: username,
+      score,
+      picture: gravatarConverter,
+    }, () => this.rankingSetter());
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div>
         <span data-testid="ranking-title">Ranking</span>
+        { loading ? <Loading /> : <this.rankingFetcher /> }
+        <Link to="/">
+          <button type="button" data-testid="btn-go-home">
+            Jogar novamente
+          </button>
+        </Link>
       </div>
     );
   }

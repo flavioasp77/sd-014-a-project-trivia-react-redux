3e279f75e59059985cd6d2f8f3bea98125fc8d4d
JosueTavaress <josueferreira8.jf@gmail.com>
2021-10-26 20:14:54 -0300

    add style ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../styles/ranking.css';
 
 export class Ranking extends Component {
   render() {
     const arrayPlayers = JSON.parse(localStorage.getItem('ranking'));
     arrayPlayers.sort((a, b) => b.score - a.score);
     const { history } = this.props;
+    const podio = 3;
     return (
-      <main>
+      <main className="main-containner-ranking">
         <h2 data-testid="ranking-title">Ranking</h2>
-        {arrayPlayers.map((item, index) => (
-          <div key={ index }>
-            <img src={ item.picture } alt="raking" />
-            <h3 data-testid={ `player-name-${index}` }>{item.name}</h3>
-            <span data-testid={ `player-score-${index}` }>{item.score}</span>
-          </div>
-        ))}
-        <button
-          onClick={ () => history.push('/') }
-          type="button"
-          data-testid="btn-go-home"
-        >
-          Voltar
-        </button>
+        <div className="containner-btn">
+          <button
+            className="btn-ranking"
+            onClick={ () => history.push('/') }
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Voltar
+          </button>
+        </div>
+        <div className="containner-podio">
+          { arrayPlayers.slice(0, podio).map((i, position) => (
+            <div className="podio-space" key={ position }>
+              <img
+                className={ `img-gravatar${position}` }
+                src={ i.picture }
+                alt="gravatar"
+              />
+              <p data-testid={ `player-name-${position}` }>{i.name}</p>
+              <div className={ `position${position}` }>
+                <p data-testid={ `player-score-${position}` }>{ i.score }</p>
+                <div className={ `img-podio${position}` } />
+              </div>
+            </div>
+          )) }
+        </div>
+        <section className="containner-raking-geral">
+          {arrayPlayers.slice(podio).map((item, index) => (
+            <div className="card-podio" key={ index + podio }>
+              <img src={ item.picture } alt="raking" />
+              <div className="infor-player-containner">
+                <h3 data-testid={ `player-name-${index + podio}` }>{item.name}</h3>
+                <span data-testid={ `player-score-${index + podio}` }>{item.score}</span>
+              </div>
+            </div>
+          ))}
+        </section>
       </main>
     );
   }

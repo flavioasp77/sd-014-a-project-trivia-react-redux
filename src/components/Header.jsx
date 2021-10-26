3e74ea05b59016977a35592682b6ff84f88ceb77
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-10-26 17:04:38 -0300

    criando css para feedback e jogo na parte do jogador

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/Header.css';
 
 class Header extends React.Component {
   render() {
     const { pictureURL, name, score } = this.props;
     return (
       <header>
-        <img
-          data-testid="header-profile-picture"
-          src={ pictureURL }
-          alt="Player gravatar"
-        />
-        <h3 data-testid="header-player-name">{ name }</h3>
-        <h3 data-testid="header-score">{ score }</h3>
+        <div className="jogado">
+          <img
+            data-testid="header-profile-picture"
+            src={ pictureURL }
+            alt="Player gravatar"
+            className="img-jogador"
+          />
+          <h3 data-testid="header-player-name">{ name }</h3>
+          <h3 data-testid="header-score">{ score }</h3>
+        </div>
       </header>
     );
   }

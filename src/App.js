d4e8f1b24719e31655a01a21d98168506add19ff
Lucas G Silveira <lucgs88@gmail.com>
2021-10-26 18:22:06 -0300

    items 16 18

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/settings" component={ Settings } />
         <Route path="/game" component={ Game } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
 
       </Switch>
     </main>

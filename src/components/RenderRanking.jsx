c85de2a5aef686ae303261c69d0851cb791b192d
Caio César Amorim <caiocavalcante063@gmail.com>
2021-10-26 15:20:26 -0300

    add ranking numbers

--- src/components/RenderRanking.jsx
+++ src/components/RenderRanking.jsx
@@ -4,11 +4,11 @@ export default class RenderRanking extends Component {
   render() {
     const playerRankings = JSON.parse(localStorage.getItem('ranking')).ranking;
     return (
-      <>
+      <ol>
         { playerRankings
           .sort((playerA, playerB) => playerB.score - playerA.score)
           .map((player, index) => (
-            <div key={ index }>
+            <li key={ index }>
               <span data-testid={ `player-name-${index}` }>{ player.name }</span>
               <span>
                 - Pontos:
@@ -17,9 +17,9 @@ export default class RenderRanking extends Component {
                   { player.score }
                 </span>
               </span>
-            </div>
+            </li>
           )) }
-      </>
+      </ol>
     );
   }
 }

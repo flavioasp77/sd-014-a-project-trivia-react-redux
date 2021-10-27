1cca45f60079fe6c7e32f41fe6622ee4fedf5b96
JosueTavaress <josueferreira8.jf@gmail.com>
2021-10-27 13:50:27 -0300

    add style feedback

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import { getStatePlayer } from '../helpers/index';
+import '../styles/feedback.css';
 
 export class Feedback extends Component {
   render() {
@@ -10,26 +11,40 @@ export class Feedback extends Component {
     const feedback = assertions >= numFeedback ? 'Mandou bem!' : 'Podia ser melhor...';
     const { history } = this.props;
     return (
-      <main>
+      <>
         <Header />
-        <h4 data-testid="feedback-text">{feedback}</h4>
-        <h4 data-testid="feedback-total-score">{score}</h4>
-        <h4 data-testid="feedback-total-question">{assertions}</h4>
-        <button
-          type="button"
-          data-testid="btn-play-again"
-          onClick={ () => history.push('/') }
-        >
-          Jogar novamente
-        </button>
-        <button
-          onClick={ () => history.push('/ranking') }
-          type="button"
-          data-testid="btn-ranking"
-        >
-          Ver Ranking
-        </button>
-      </main>
+        <main className="containner-main-feedback">
+          <div className="card-feedback">
+            <div className="card-text-contains">
+              <h4 data-testid="feedback-text">{feedback}</h4>
+            </div>
+            <div className="card-text-contains">
+              <h4>Score:</h4>
+              <h4 data-testid="feedback-total-score">{score}</h4>
+            </div>
+            <div className="card-text-contains">
+              <h4>successes:</h4>
+              <h4 data-testid="feedback-total-question">{assertions}</h4>
+            </div>
+            <div className="containner-btn-feedback">
+              <button
+                type="button"
+                data-testid="btn-play-again"
+                onClick={ () => history.push('/') }
+              >
+                Jogar novamente
+              </button>
+              <button
+                onClick={ () => history.push('/ranking') }
+                type="button"
+                data-testid="btn-ranking"
+              >
+                Ver Ranking
+              </button>
+            </div>
+          </div>
+        </main>
+      </>
     );
   }
 }
